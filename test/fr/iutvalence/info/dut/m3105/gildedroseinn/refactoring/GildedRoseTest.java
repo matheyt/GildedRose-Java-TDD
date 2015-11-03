3d33310b6b247cc6b1a87a43430641a6f6a91be3
matheyt <matheyt@PC-B124-06.iut-valence.fr>
2015-11-03 13:55:46 +0100

    Red test5 updateItem test : max quality is 50

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -71,4 +71,16 @@ public class GildedRoseTest
 		assertEquals(0,items.get(1).getQuality());
 	}
 	
+	@Test
+	public void testLimiteMaxQuality()
+	{
+		ArrayList<Item> items = new ArrayList<Item>();
+		items.add(new Item("Aged Brie", 10, 50));
+		items.add(new Item("Bonjour", -2, 0));
+		GildedRose.updateItems(items);
+		assertEquals(9,items.get(0).getSellIn());
+		assertEquals(50,items.get(0).getQuality());
+		assertEquals(-3,items.get(1).getSellIn());
+		assertEquals(0,items.get(1).getQuality());
+	}
 }
