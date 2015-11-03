a0f92de771647a321eba6c66793a14a1a3b1909a
matheyt <matheyt@PC-B124-06.iut-valence.fr>
2015-11-03 13:27:29 +0100

    Red test2 updateItem When SellIn may be negative => Quality decreases by 2/day

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -22,14 +22,27 @@ public class GildedRoseTest
 	@Test
 	public void testLoweringValuesManyItem()
 	{
-		ArrayList<Item> items = new ArrayList<Item>(); 
-		items.add(new Item("+5 Dexterity Vest", 10, 10)); 
-		items.add(new Item("Aged Brie", 0, 0)); 
+		ArrayList<Item> items = new ArrayList<Item>();
+		items.add(new Item("Robert", 10, 10));
+		items.add(new Item("Bonjour", 0, 0));
 		GildedRose.updateItems(items);
 		assertEquals(9,items.get(0).getSellIn());
 		assertEquals(9,items.get(0).getQuality());
 		assertEquals(-1,items.get(1).getSellIn());
 		assertEquals(-1,items.get(1).getQuality());
+	}
+	
+	@Test
+	public void testLoweringValuesNegativeSellIn()
+	{
+		ArrayList<Item> items = new ArrayList<Item>();
+		items.add(new Item("+5 Dexterity Vest", 10, 10));
+		items.add(new Item("Aged Brie", -2, 10));
+		GildedRose.updateItems(items);
+		assertEquals(9,items.get(0).getSellIn());
+		assertEquals(9,items.get(0).getQuality());
+		assertEquals(-3,items.get(1).getSellIn());
+		assertEquals(8,items.get(1).getQuality());
 	}
 	
 }
