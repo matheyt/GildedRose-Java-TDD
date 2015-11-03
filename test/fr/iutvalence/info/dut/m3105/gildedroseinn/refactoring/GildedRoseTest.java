d46c614d7c33dfa750d7dcb63d16613c5c8390a7
matheyt <matheyt@PC-B124-06.iut-valence.fr>
2015-11-03 13:23:34 +0100

    Green test1 updateItem first version => all values are decreased by 1 each day

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -2,6 +2,9 @@ package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class GildedRoseTest
@@ -14,5 +17,19 @@ public class GildedRoseTest
 		assertEquals(3,item.getSellIn());
 		assertEquals(2,item.getQuality());
 	}
-
+	
+	
+	@Test
+	public void testLoweringValuesManyItem()
+	{
+		ArrayList<Item> items = new ArrayList<Item>(); 
+		items.add(new Item("+5 Dexterity Vest", 10, 10)); 
+		items.add(new Item("Aged Brie", 0, 0)); 
+		GildedRose.updateItems(items);
+		assertEquals(9,items.get(0).getSellIn());
+		assertEquals(9,items.get(0).getQuality());
+		assertEquals(-1,items.get(1).getSellIn());
+		assertEquals(-1,items.get(1).getQuality());
+	}
+	
 }
